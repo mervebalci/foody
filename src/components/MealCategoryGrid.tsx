0010fbb95c03fa3f13bb425d95db0ac5bf864053
Merve Balci <mervebalci090@gmail.com>
2024-11-06 16:24:00 -0400

    Creating a custom hook to fetch the meals

--- src/components/MealCategoryGrid.tsx
+++ src/components/MealCategoryGrid.tsx
@@ -1,28 +1,8 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
 import { Text } from "@chakra-ui/react";
-
-interface MealCategory {
-  idCategory: string;
-  strCategory: string;
-  strCategoryDescription: string;
-  strCategoryThumb: string;
-}
-
-interface FetchCategoriesResponse {
-  categories: MealCategory[];
-}
+import useMeals from "../hooks/useMeals";
 
 export default function MealCategoryGrid() {
-  const [meals, setMeals] = useState<MealCategory[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get<FetchCategoriesResponse>("/categories.php")
-      .then((res) => setMeals(res.data.categories))
-      .catch((err) => setError(err.message));
-  }, []);
+  const { meals, error } = useMeals();
 
   return (
     <>
---
+++ src/hooks/useMeals.ts
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+interface MealCategory {
+  idCategory: string;
+  strCategory: string;
+  strCategoryDescription: string;
+  strCategoryThumb: string;
+}
+
+interface FetchCategoriesResponse {
+  categories: MealCategory[];
+}
+
+const useMeals = () => {
+  const [meals, setMeals] = useState<MealCategory[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    apiClient
+      .get<FetchCategoriesResponse>("/categories.php", { signal: signal })
+      .then((res) => setMeals(res.data.categories))
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message)
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  return { meals, error }
+}
+
+export default useMeals;
